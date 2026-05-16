175c0c239c584af70a35c01e8b032b708295a8f7
CS041 Faatih <22oo1cso41faatih@gmail.com>
2026-05-16 21:19:21 +0530

    feat(ui): add custom 404 and 500 pages with dotlottie animations

---
+++ components/NotFoundAnimation.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+export default function NotFoundAnimation() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-48 h-48 sm:w-64 sm:h-64 md:w-72 md:h-72 lg:w-80 lg:h-80">
+        <DotLottieReact
+          src="https://lottie.host/a78410a6-e8ed-443c-9566-14734cc1d2c9/CbfCBwn8X5.lottie"
+          loop
+          autoplay
+        />
+      </div>
+    </div>
+  );
+}
